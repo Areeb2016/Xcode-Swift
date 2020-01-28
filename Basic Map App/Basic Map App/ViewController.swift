444b75b4d7b4ff3bdd265ad9077f116a352c2528
BitesDigital <BitesDigital@users.noreply.github.com>
2020-01-28 22:43:38 +0500

    Third Commit

---
+++ Basic Map App/Basic Map App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic Map App
+//
+//  Created by Digital Bites on 28/01/2020.
+//  Copyright © 2020 Digital Bites. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
